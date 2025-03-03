0f829676a1df4f0ae48592fab6b938df792be453
kevinho10 <kevinho10@github.com>
2025-03-03 05:19:09 -0800

    adjust attn shape

--- self_attention.py
+++ self_attention.py
@@ -7,7 +7,7 @@ class self_attention(nn.Module):
 
     def __init__(self, in_channels, out_channels, dk, dq, dv, Nh):
         super(self_attention, self).__init__()
-        self.Cin = in_channels
+        self.Cin = in_channels * 3
         self.Cout = out_channels
         self.dq = dq
         self.dk = dk
