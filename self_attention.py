d24d356dacdf1f1ba9f5bf93009e323997bef883
kevinho10 <kevinho10@github.com>
2025-03-03 05:46:09 -0800

    undo self attention dimension changes

--- self_attention.py
+++ self_attention.py
@@ -7,7 +7,7 @@ class self_attention(nn.Module):
 
     def __init__(self, in_channels, out_channels, dk, dq, dv, Nh):
         super(self_attention, self).__init__()
-        self.Cin = in_channels 
+        self.Cin = in_channels
         self.Cout = out_channels
         self.dq = dq
         self.dk = dk
@@ -50,8 +50,7 @@ class self_attention(nn.Module):
 
     def forward(self, inputs):
         batch, N, H, W = inputs.shape
-        N = N * 3
-        print(f'n - {N} attn in - {inputs.shape}')
+        #print(inputs.shape)
         flat_q, flat_k, flat_v, q, k, v = self.compute_flat_qkv(inputs, self.q, self.k,self.v,self.Nh)
         #print(flat_q.shape)
         logits = torch.matmul(flat_q.transpose(2, 3), flat_k)
@@ -67,7 +66,7 @@ class self_attention(nn.Module):
         #print(attn_out.shape)
         attn_out = self.attn(attn_out)
         #print(attn_out.shape)
-        print(f'attnout {attn_out.shape}')
+
         return attn_out
 
 
