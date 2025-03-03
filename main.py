70a13dbd812ded68af33b970e690b51821041ef0
kevinho10 <kevinho10@github.com>
2025-03-03 06:57:48 -0800

    check net state first

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def main():
     pretrained = torch.load(args.weight_path, map_location=torch.device('cpu'))
 
     for k, v in net.state_dict().items():
-        print(f'k: {k[9:]} pre {pretrained.module.state_dict()[k[9:]]}')
+        print(f'k: {k[9:]} ')
         if "xx" in k:
             print(re.sub(r'xx[0-9]\.?',".", k[9:]))
 
